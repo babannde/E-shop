751d48b1b7b17da2f59d57493a7a6f8443239b3c
Hamza Baba <hamzabande@outlook.com>
2023-08-30 12:59:19 +0100

    frontend url done

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://e-shop-z4b1.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
