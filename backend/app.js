f3f85f4751673f360f645fd8ce3cf99c38ae7b16
Hamza Baba <hamzabande@outlook.com>
2023-09-01 08:09:42 +0100

    frontend patch done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://e-shop-z4b1.vercel.app',],
+  origin: ['https://kiyira.com',],
   credentials: true
 }));
 
