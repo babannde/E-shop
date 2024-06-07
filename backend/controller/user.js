ed8fcb7f0b8f3e69516c22ed0ac4c656ab2a7da9
Hamza Tope Babande <hamzabande@outlook.com>
2024-06-07 11:46:27 +0100

    Update user.js
    Cors link update

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://kiyira.com/activation/${activationToken}`;
+    const activationUrl = `https://www.kiyira.com/activation/${activationToken}`;
 
     try {
       await sendMail({
