3d110caf94a3a90938a0fff0d12c1f0a1377dacb
Hamza Tope Babande <hamzabande@outlook.com>
2024-06-07 11:44:19 +0100

    Update app.js
    Cors link update

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://kiyira.com',],
+  origin: ['https://www.kiyira.com',],
   credentials: true
 }));
 
